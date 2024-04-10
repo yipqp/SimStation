b5aa769228dcc4a6103f8ca698e518a2bfd5ffc6
Ethan Le <ethanisverybusyrightnow@gmail.com>
2024-04-10 11:06:50 -0700

    Filled in Heading class

--- src/simstation/Heading.java
+++ src/simstation/Heading.java
@@ -1,4 +1,26 @@
 package src.simstation;
+import src.mvc.*;
 
-public class Heading {
+public enum Heading {
+
+    NORTH, EAST, SOUTH, WEST;
+
+    public static Heading parse(String heading) {
+        if (heading.equalsIgnoreCase("north")) return NORTH;
+        if (heading.equalsIgnoreCase("east")) return EAST;
+        if (heading.equalsIgnoreCase("south")) return SOUTH;
+        if (heading.equalsIgnoreCase("west")) return WEST;
+        Utilities.error("Invalid heading: " + heading);
+        return null;
+
+    }
+
+    public static Heading random() {
+        int luck = Utilities.rng.nextInt(4);
+        if(luck == 0) return NORTH;
+        if(luck == 1) return SOUTH;
+        if(luck == 2) return EAST;
+        return WEST;
+
+    }
 }
